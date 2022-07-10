ed379ab04399ed34f718c90d27a51d6469404ff5
hmdyt <221s120s@stu.kobe-u.ac.jp>
2022-07-10 14:53:02 +0900

    output state off feature

--- miraclue_low_voltage_monitor/set_voltage.py
+++ miraclue_low_voltage_monitor/set_voltage.py
@@ -2,6 +2,7 @@ import argparse
 import time
 
 import miraclue_low_voltage_monitor.config as config
+import miraclue_low_voltage_monitor.util as util
 from miraclue_low_voltage_monitor.IT6322A_handler import IT6322A_handler
 
 def main():
@@ -9,6 +10,7 @@ def main():
     parser.add_argument("v_ch1", type=float)
     parser.add_argument("v_ch2", type=float)
     parser.add_argument("v_ch3", type=float)
+    parser.add_argument("--state", "-s", type=int, default=1)
     args = parser.parse_args()
     
     handler = IT6322A_handler(
@@ -19,8 +21,13 @@ def main():
     handler.set_voltage(1, args.v_ch2)
     handler.set_voltage(2, args.v_ch3)
     time.sleep(1)
+
     voltages = [handler.get_voltage(i) for i in range(3)]
-    print(f"current voltages are {voltages}")
+    util.tprint(f"current voltages are {voltages}")
+
+    handler.set_state(int(args.state))
+    output_state = handler.get_state()
+    util.tprint(f"output state is now {output_state}")
 
 if __name__ == '__main__':
     main()
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-docker-compose exec -T monitor poetry run python miraclue_low_voltage_monitor/set_voltage.py 0 0 0
+docker-compose exec -T monitor poetry run python miraclue_low_voltage_monitor/set_voltage.py --state 0 0 0 0
 docker-compose down
